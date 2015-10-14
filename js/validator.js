ab6b20a7db28a615181ca29ecf43a1deb7b20505
xiongjianqiao <xiongjianqiao@fangpinhui.com>
2015-10-14 15:30:35 +0800

    添加表单插件

---
+++ js/validator.js
@@ -0,0 +1,45 @@
+// 表单验证插件
+var FormValidator=function(options){
+	this.items=[];
+}
+
+FormValidator.prototype.add=function(opt){
+	var items=this.items;
+	if(opt.length){//数组形式插入
+		for(var i=0;i<opt.length;i++){
+			items.push(opt[i]);
+		}
+	}
+	else{
+		items.push(opt);
+	}
+	
+}
+
+FormValidator.prototype.valid=function(str,regName){
+	var items=this.items;
+	var idx=0;
+	for(var i=0;i<items.length;i++){
+		if(items[i].name===regName){
+			idx=i;
+			break;
+		}
+		if(i>=items.length-1){
+			console.error('please use add function to get the \''+regName+'\' first!');
+			return false;
+		}
+	}
+	var reg=items[i].reg;
+	var msg=items[i].msg;
+	if(reg['required']){
+		if(str==''){
+			return msg['required'];
+		}
+	}
+	for(var i in reg){
+		if(i!='required' && !reg[i].test(str)){
+			return msg[i];
+		}
+	}
+	return true;
+}
