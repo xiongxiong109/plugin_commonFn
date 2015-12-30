c115c0e8b406fd6a6c2c5e8327c9bd4c481f7981
xiongjianqiao <xiongjianqiao@fangpinhui.com>
2015-12-30 12:35:23 +0800

    更新弹出层定位

--- js/plugins/zepto.overlay.js
+++ js/plugins/zepto.overlay.js
@@ -9,21 +9,12 @@
 		str=str || '';
 		$("#box").find('p').text(str.toString());
 
-		$("#box").css({
-			'opacity':0,
-			'position':'fixed',
-			'transform':'scale(0) translateY(-50%)',
-			'-webkit-transform':'scale(0) translateY(-50%)',
-			'top':'50%'
-		});
-
 		$("#overlay").fadeIn(200,showBox);
 
 		function hideBox(){
 				$("#box").animate({
 					'opacity':0,
-					'scale':0,
-					'translateY':'-50%'
+					'scale':0
 				},200,'ease-in',function(){
 					$("#overlay").fadeOut(function(){
 						$("#overlay").hide();
@@ -36,8 +27,7 @@
 			$("#box").animate({
 				'opacity':1,
 				'scale':1,
-				'translateY':'-50%'
-			},300,'ease',function(){
+			},200,'ease-out',function(){
 				//弹出框点击按钮
 				$("#confirm").one(clickEvent,hideBox);
 			});
